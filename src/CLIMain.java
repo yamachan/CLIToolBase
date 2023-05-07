a657e695bad45748a958c07e7b9f7c027fff3c5d
yamachan <z@rinco.jp>
2023-05-07 22:46:46 +0900

    Add commond-cli jar and simple test code.

--- src/CLIMain.java
+++ src/CLIMain.java
@@ -1,9 +1,25 @@
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class CLIMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World.");
-	}
+	public static void main(String[] args) throws ParseException {
+		// create Options object
+		Options options = new Options();
 
+		// add t option
+		options.addOption("t", false, "display current time");
+		
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = parser.parse(options, args);
+
+		if(cmd.hasOption("t")) {
+			System.out.println("Sample output with t option.");		
+		} else {
+			System.out.println("Sample output without t option.");		
+		}
+	}
 }
